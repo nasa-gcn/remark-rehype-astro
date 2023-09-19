a6cbde4621ec44fabb61c1fba2a976da1fcd3582
Leo Singer <leo.singer@ligo.org>
2023-09-19 14:45:49 -0400

    Parse GCN Circular URLs

--- src/replacements/gcn-circular/index.ts
+++ src/replacements/gcn-circular/index.ts
@@ -9,13 +9,22 @@ const preamble = /GCN(?:s|\s+Circulars?|\s+Circs?\.)?\s*/.source
 const conjunction = /(?:\s*|,|and)+/.source
 const circularIdRegExp = /\d+(?:a|\.5)?/gi
 const circularId = circularIdRegExp.source
+const legacyUrlOrigin = /https?:\/\/gcn\.gsfc\.nasa\.gov/.source
+const legacyUrlExtension = /\.gcn3/.source
+const legacyUrl = `${legacyUrlOrigin}/gcn3/(${circularId})${legacyUrlExtension}`
+const urlOrigin = /https?:\/\/gcn.nasa.gov/.source
+const url = `${urlOrigin}/circulars/(${circularId})`
 
 export default {
   find: new RegExp(
-    `${preamble}${circularId}(?:${conjunction}${circularId})*`,
+    `${url}|${legacyUrl}|${preamble}${circularId}(?:${conjunction}${circularId})*`,
     'gi'
   ),
-  replace(data, value) {
+  replace(data, value, circularIdFromUrl, circularIdFromLegacyUrl) {
+    circularIdFromUrl ||= circularIdFromLegacyUrl
+    if (circularIdFromUrl)
+      return data('gcn-circular', value, normalize(circularIdFromUrl))
+
     const result = []
     let lastIndex = 0
     for (const match of value.matchAll(circularIdRegExp)) {
