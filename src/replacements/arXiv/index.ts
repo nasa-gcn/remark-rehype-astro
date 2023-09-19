11c6a021818944cdc802230728620450ef08561c
Leo Singer <leo.singer@ligo.org>
2023-09-19 14:56:24 -0400

    Parse arXiv URLs

--- src/replacements/arXiv/index.ts
+++ src/replacements/arXiv/index.ts
@@ -1,7 +1,10 @@
 import type { FindAndReplace } from '../index.js'
 
+const marker = /(?:arXiv:|https?:\/\/(?:www\.)?arxiv\.org\/abs\/)/.source
+const articleId = /(?:\d{4}\.\d{5}|[a-z-]+\/\d{7})(?:v\d+)?/.source
+
 export default {
-  find: /arXiv:((?:\d{4}\.\d{5}|[a-z-]+\/\d{7})(?:v\d+)?)/g,
+  find: new RegExp(`${marker}(${articleId})`, 'g'),
   replace(data, text, value) {
     return data('arXiv', text, value)
   },
