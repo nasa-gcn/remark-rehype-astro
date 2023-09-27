4daa7d74b3ae724b7f9e2d06561ca4f6712c5372
Leo Singer <leo.singer@ligo.org>
2023-09-27 12:50:37 -0400

    Use lower-case `arxiv` as class name
    For consistency.

--- src/replacements.ts
+++ src/replacements.ts
@@ -1,11 +1,11 @@
-import arXiv from './replacements/arXiv/index.js'
+import arxiv from './replacements/arxiv/index.js'
 import datetime from './replacements/datetime/index.js'
 import doi from './replacements/doi/index.js'
 import gcnCircular from './replacements/gcn-circular/index.js'
 import type { FindAndReplace } from './replacements/index.js'
 
 export const replacements: FindAndReplace[] = [
-  arXiv,
+  arxiv,
   datetime,
   doi,
   gcnCircular,
--- src/replacements/arxiv/index.ts
+++ src/replacements/arxiv/index.ts
@@ -6,6 +6,6 @@ const articleId = /(?:\d{4}\.\d{5}|[a-z-]+\/\d{7})(?:v\d+)?/.source
 export default {
   find: new RegExp(`${marker}(${articleId})`, 'g'),
   replace(data, text, value) {
-    return data('arXiv', text, value)
+    return data('arxiv', text, value)
   },
 } satisfies FindAndReplace
