6bb6f90a1cdd62fcd110a3ba44f9fd1fd6548301
tylerbarna <tylerpbarna@gmail.com>
2024-06-12 12:13:34 -0500

    fix regex for tns links
    tns object strings located in URLs will no longer be linked to, fixing issues like the current bug in Circular 31683

--- src/replacements/tns/index.ts
+++ src/replacements/tns/index.ts
@@ -1,7 +1,7 @@
 import type { FindAndReplace } from '../index.js'
 
 export default {
-  find: /(?<!http:\/\/|https:\/\/)AT\s*(2\d{3}[a-z]+)/g,
+  find: /(?<!http:\/\/|https:\/\/)(?<!http:\/\/.*|https:\/\/.*)AT\s*(2\d{3}[a-z]+)/g,
   replace(data, text, value) {
     return data('tns', text, value)
   },
