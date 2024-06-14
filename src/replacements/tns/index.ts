7462fe96fc9fc31838cf937966efd60b55929f3a
Tyler Barna <57914086+tylerbarna@users.noreply.github.com>
2024-06-14 14:11:21 -0400

    simplify regular expression
    from Dakota's review on 06/13/24
    
    Co-authored-by: Dakota Dutko <dakota.d@live.com>

--- src/replacements/tns/index.ts
+++ src/replacements/tns/index.ts
@@ -1,7 +1,7 @@
 import type { FindAndReplace } from '../index.js'
 
 export default {
-  find: /(?<!http:\/\/|https:\/\/)(?<!http:\/\/.*|https:\/\/.*)AT\s*(2\d{3}[a-z]+)/g,
+  find: /(?<!https?:\/\/)(?<!https?:\/\/.*)AT\s*(2\d{3}[a-z]+)/g,
   replace(data, text, value) {
     return data('tns', text, value)
   },
